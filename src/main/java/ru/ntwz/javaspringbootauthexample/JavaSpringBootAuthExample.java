24f9568780191b624ad7c313f324bdcd34fb6b68
notweuz <weuz.main@gmail.com>
2025-07-12 16:45:49 +0400

    Initial commit

---
+++ src/main/java/ru/ntwz/javaspringbootauthexample/JavaSpringBootAuthExample.java
@@ -0,0 +1,11 @@
+package ru.ntwz.javaspringbootauthexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootAuthExample {
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringBootAuthExample.class, args);
+    }
+}
