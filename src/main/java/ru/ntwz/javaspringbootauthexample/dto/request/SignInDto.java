e395c53dbe3e725a2783db836038b24203d887b4
notweuz <weuz.main@gmail.com>
2025-07-12 18:30:04 +0400

    Add CAN_NOT_BE_BLANK usage into sign in and sign up dto

--- src/main/java/ru/ntwz/javaspringbootauthexample/dto/request/SignInDto.java
+++ src/main/java/ru/ntwz/javaspringbootauthexample/dto/request/SignInDto.java
@@ -1,5 +1,6 @@
 package ru.ntwz.javaspringbootauthexample.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -14,10 +15,12 @@ public class SignInDto {
     @Size(min = ValidationConstant.Length.NAME_MIN, max = ValidationConstant.Length.NAME_MAX,
             message = "Username" + ValidationConstant.Message.WRONG_LENGTH)
     @NotNull(message = "Username" + ValidationConstant.Message.REQUIRED)
+    @NotBlank(message = "Username" + ValidationConstant.Message.CAN_NOT_BE_BLANK)
     private String username;
 
     @Size(min = ValidationConstant.Length.PASSWORD_MIN, max = ValidationConstant.Length.PASSWORD_MAX,
             message = "Password" + ValidationConstant.Message.WRONG_LENGTH)
     @NotNull(message = "Password" + ValidationConstant.Message.REQUIRED)
+    @NotBlank(message = "Username" + ValidationConstant.Message.CAN_NOT_BE_BLANK)
     private String password;
 }
--- src/main/java/ru/ntwz/javaspringbootauthexample/dto/request/SignUpDto.java
+++ src/main/java/ru/ntwz/javaspringbootauthexample/dto/request/SignUpDto.java
@@ -1,5 +1,6 @@
 package ru.ntwz.javaspringbootauthexample.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -16,10 +17,12 @@ public class SignUpDto {
             message = "Username" + ValidationConstant.Message.WRONG_LENGTH)
     @NotNull(message = "Username" + ValidationConstant.Message.REQUIRED)
     @Pattern(regexp = ValidationConstant.Regex.NAME_REGEX, message = "Username" + ValidationConstant.Message.INCORRECT_FORMAT)
+    @NotBlank(message = "Username" + ValidationConstant.Message.CAN_NOT_BE_BLANK)
     private String username;
 
     @Size(min = ValidationConstant.Length.PASSWORD_MIN, max = ValidationConstant.Length.PASSWORD_MAX,
             message = "Password" + ValidationConstant.Message.WRONG_LENGTH)
     @NotNull(message = "Password" + ValidationConstant.Message.REQUIRED)
+    @NotBlank(message = "Username" + ValidationConstant.Message.CAN_NOT_BE_BLANK)
     private String password;
 }
